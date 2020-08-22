912c4996f59ab9bbe326551f7b61a327be3ff8a9
Logan Montalto <64017390+Logan-Montalto@users.noreply.github.com>
2020-08-22 14:00:11 +0200

    Update site_prof.js

--- Frontend/js/site_prof.js
+++ Frontend/js/site_prof.js
@@ -6,67 +6,76 @@
  * @date 09-July-2020
  */
 
-var professeurs = {
-    P01: {Nom: 'Dumbledore', Prenom: 'Albus', Niveau: "Supérieur", Matieres:"Psychologie", Prix: 14},
-    P02: {Nom: 'Farnsworth', Prenom: 'Hubert', Niveau: "Primaire", Matieres: "Mathématiques", Prix: 7},
-    P03: {Nom: 'Hoover', Prenom: 'Elizabeth', Niveau: "Primaire", Matieres: "Français", Prix: 8},
-    P04: {Nom: 'Jones', Prenom: 'Indiana', Niveau: "Supérieur", Matieres:"Géographie", Prix: 9},
-    P05: {Nom: 'Langdon', Prenom: 'Robert', Niveau: "Supérieur", Matieres:"Histoire", Prix: 7},
-    P06: {Nom: 'McGonagall', Prenom: 'Minerva', Niveau: "Supérieur", Matieres:"Droit", Prix: 11},
-    P07: {Nom: 'Suresh', Prenom: 'Mohinder', Niveau: "Supérieur", Matieres:"Chimie", Prix: 10},
-    P08: {Nom: 'Mosby', Prenom: 'Ted', Niveau: "Supérieur", Matieres:"Architecture", Prix: 13},
-    P09: {Nom: 'Tournesol', Prenom: 'Tryphon', Niveau: "Secondaire", Matieres:"Sciences", Prix: 9},
-    P10: {Nom: 'Xavier', Prenom: 'Charles', Niveau: "Supérieur", Matieres:"Biologie", Prix: 15},
+var primaire = {
+    P01: {Nom: 'Farnsworth', Prenom: 'Hubert', Niveau: "Primaire", Matieres: "Mathématiques", Prix: 7},
+    P02: {Nom: 'Hoover', Prenom: 'Elizabeth', Niveau: "Primaire", Matieres: "Français", Prix: 8},
     };
+
+var secondaire = {
+    SE01: {Nom: 'Tournesol', Prenom: 'Tryphon', Niveau: "Secondaire", Matieres:"Sciences", Prix: 9},
+};
+
+var superieur = {
+    SU01: {Nom: 'Dumbledore', Prenom: 'Albus', Niveau: "Supérieur", Matieres:"Psychologie", Prix: 14},
+    SU02: {Nom: 'Jones', Prenom: 'Indiana', Niveau: "Supérieur", Matieres:"Géographie", Prix: 9},
+    SU03: {Nom: 'Langdon', Prenom: 'Robert', Niveau: "Supérieur", Matieres:"Histoire", Prix: 7},
+    SU04: {Nom: 'McGonagall', Prenom: 'Minerva', Niveau: "Supérieur", Matieres:"Droit", Prix: 11},
+    SU05: {Nom: 'Suresh', Prenom: 'Mohinder', Niveau: "Supérieur", Matieres:"Chimie", Prix: 10},
+    SU06: {Nom: 'Mosby', Prenom: 'Ted', Niveau: "Supérieur", Matieres:"Architecture", Prix: 13},
+    SU07: {Nom: 'Xavier', Prenom: 'Charles', Niveau: "Supérieur", Matieres:"Biologie", Prix: 15},
+};
+
+var niveau = [primaire, secondaire, superieur];
+
 
 // Liste déroulante dynamique
 
 function Choix(form) {
-    let i = form.Niveau.selectedIndex;
-    if (i === 0) {
-        for (i=0;i<8;i++) {
-            form.Matieres.options[i].text="";
-        }
-        return;
-    }
+    let n = form.Niveau.selectedIndex;
     form.Matieres.selectedIndex = 0;
-    switch (i) {
+    switch (n) {
         case 1 :
-            var txt = new Array ('Français','Mathématiques');
-            form.Matieres.options[0].text="-- Choisissez une matière ---";
+             document.formMatiere.Matieres.innerHTML = "<option>--- Choisissez une matière ---</option>";
             var f=document.formMatiere.Matieres;
-            for (i=0;i<txt.length;i++) {
+            for (let i in primaire) {
                 f.length++;
-                f.options[f.length-1].text = txt[i];
-                form.Matieres.options[i+1].text=txt[i];
+                f.options[f.length-1].text = primaire[i].Matieres;
             }
             break;
         case 2 :
-            var txt = new Array ('Sciences');
-            form.Matieres.options[0].text="-- Choisissez une matière ---";
+            document.formMatiere.Matieres.innerHTML = "<option>--- Choisissez une matière ---</option>";
             var d=document.formMatiere.Matieres;
-            for (i=0;i<txt.length;i++) {
+            for (let i in secondaire) {
                 d.length++;
-                d.options[d.length-1].text = txt[i];
-                form.Matieres.options[i+1].text=txt[i];
+                d.options[d.length-1].text = secondaire[i].Matieres;
             }
             break;
         case 3 :
-            var txt = new Array ('Architecture','Biologie','Chimie', 'Droit', 'Géographie', 'Histoire', 'Psychologie');
-            form.Matieres.options[0].text="-- Choisissez une matière ---";
+            document.formMatiere.Matieres.innerHTML = "<option>--- Choisissez une matière ---</option>";
             var c=document.formMatiere.Matieres;
-            for (i=0;i<txt.length;i++) {
+            for (let i in superieur) {
                 c.length++;
-                c.options[c.length-1].text = txt[i];
-                form.Matieres.options[i+1].text=txt[i];
+                c.options[c.length-1].text = superieur[i].Matieres;
             }
             break;
     }
 }
 
-var coursParticuliers = new Object();
 
-function initialiserPage() {
-    coursParticuliers = {};
-    document.querySelectorAll("section")[1].innerHTML = "<table><thead><th>Nom de l'élève</th><th>Date du cours</th><th>Professeur</th><th>Nombre d'heures de cours</th><th>Prix Total</th></thead><tbody id='cours_particuliers'></tbody></table>";
+function Prof(form){
+    document.formMatiere.Profs.innerHTML = "<option>--- Choisissez un professeur ---</option>";
+    let matieresShort = document.formMatiere.Matieres;
+    let matieresSelect = matieresShort.options[matieresShort.selectedIndex].text;
+
+    let niveauShort = document.formMatiere.Niveau;
+    let niveauSelect = niveau[niveauShort.selectedIndex-1];
+    console.log(niveau[niveauShort.selectedIndex-1]);
+    let y = document.formMatiere.Profs;
+
+    for (let a in niveauSelect) {
+        if (niveauSelect[a].Matieres === matieresSelect) {
+            y.length++;
+            y.options[y.length-1].text = niveauSelect[a].Nom + " " + niveauSelect[a].Prenom;
+        }
+    }
 }
